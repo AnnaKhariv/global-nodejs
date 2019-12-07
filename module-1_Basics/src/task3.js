8bc27ef94288d7c79ee2a48d9b94dfffd086d61d
Anna.Khariv <Anna_Khariv@epam.com>
2019-12-07 14:48:16 +0200

    delete exit

--- module-1_Basics/src/task3.js
+++ module-1_Basics/src/task3.js
@@ -12,19 +12,13 @@ readable
   .on('open', () => console.info(`Start reading data from ${inputPath}`))
   .on('end', () => console.info(`Finish reading data from ${inputPath}`))
   .on('close', () => console.info('Reading stream has been destroyed and file has been closed'))
-  .on('error', (err) => {
-    console.error(`Unable to read data from ${inputPath}`, err);
-    process.exit(0);
-  });
+  .on('error', (err) => console.error(`Unable to read data from ${inputPath}`, err));
 
 writable
   .on('open', () => console.info(`Start writing data to ${outputPath}`))
   .on('finish', () => console.info(`Finish writing data to ${outputPath}`))
   .on('close', () => console.info('Writing stream has been destroyed and file has been closed'))
-  .on('error', (err) => {
-    console.error(`Unable to write data to ${outputPath}`, err);
-    process.exit(0);
-  });
+  .on('error', (err) => console.error(`Unable to write data to ${outputPath}`, err);
 
 readable.pipe(csvtojson()).pipe(writable);
 
