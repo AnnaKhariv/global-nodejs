8e2122be335a0ee71f234a7b0b679742d510f358
Anna.Khariv <Anna_Khariv@epam.com>
2019-12-07 14:20:30 +0200

    task3: es6

--- module-1_Basics/src/task3.js
+++ module-1_Basics/src/task3.js
@@ -1,43 +1,55 @@
 //2
-import * as fs from 'fs';
-import * as csvtojson from 'csvtojson';
+import {createReadStream, createWriteStream} from 'fs';
+const csvtojson = require ('csvtojson');
 
-const inputPath ='./csv/node_mentoring_t1_2_input_example.csv';
-const outputPath ='./txt/node_mentoring_t1_2_outut_example.txt';
+const inputPath ='./dist/csv/node_mentoring_t1_2_input_example.csv';
+const outputPath ='./dist/txt/node_mentoring_t1_2_outut_example.txt';
 
-const readable = fs.createReadStream(inputPath);
-const writable = fs.createWriteStream(outputPath);
+const readable = createReadStream(inputPath);
+const writable = createWriteStream(outputPath);
 
-readable.on('open', () => {
-  console.info(`Start reading data from ${inputPath}`);
-});
+readable
+  .on('open', () => console.info(`Start reading data from ${inputPath}`))
+  .on('end', () => console.info(`Finish reading data from ${inputPath}`))
+  .on('close', () => console.info('Reading stream has been destroyed and file has been closed'))
+  .on('error', (err) => {
+    console.error(`Unable to read data from ${inputPath}`, err);
+    process.exit(0);
+  });
 
+writable
+  .on('open', () => console.info(`Start writing data to ${outputPath}`))
+  .on('finish', () => console.info(`Finish writing data to ${outputPath}`))
+  .on('close', () => console.info('Writing stream has been destroyed and file has been closed'))
+  .on('error', (err) => {
+    console.error(`Unable to write data to ${outputPath}`, err);
+    process.exit(0);
+  });
 
 readable.pipe(csvtojson()).pipe(writable);
-// //1
-// const encoding = 'utf8';
-// process.stdin.setEncoding(encoding);
 
-// const reverse = (data) => {
-//   const reversedData = [];
-//   const array = data.split('');
-//   array.pop();
+//1
+const encoding = 'utf8';
+process.stdin.setEncoding(encoding);
 
-//   for(let i = 0; i < array.length; i++) {
-//     reversedData.unshift(array[i]);
-//   }
+const reverse = (data) => {
+  const reversedData = [];
+  const array = data.split('');
+  array.pop();
 
-//   return reversedData.join('');
-// };
+  for(let i = 0; i < array.length; i++) {
+    reversedData.unshift(array[i]);
+  }
 
-// process.stdin.on('readable', (chunk) => {
-//   while ((chunk = process.stdin.read()) !== null) {
-//     process.stdout.write(`${reverse(chunk)}\n\n`);
-//   }
-// });
+  return reversedData.join('');
+};
 
-// process.stdin.on('end', () => {
-//   process.stdout.write('end');
-// });
+process.stdin.on('readable', (chunk) => {
+  while ((chunk = process.stdin.read()) !== null) {
+    process.stdout.write(`${reverse(chunk)}\n\n`);
+  }
+});
 
-
+process.stdin.on('end', () => {
+  process.stdout.write('end');
+});
